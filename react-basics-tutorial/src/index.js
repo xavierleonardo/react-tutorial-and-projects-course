721e8dc3d32ce80ed5f4729c64969db5e282c4c4
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 11:32:28 -0300

    react: Fisrt Component

---
+++ react-basics-tutorial/src/index.js
@@ -0,0 +1,7 @@
+import ReactDOM from "react-dom";
+
+function Greeting() {
+  return <h4>this is xavier and this is my first component</h4>;
+}
+
+ReactDOM.render(<Greeting />, document.getElementById("root"));
