59ade76ad34f30c362849bc162e47045f32e90ed
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 14:53:45 -0300

    react: Proper List

--- react-basics-tutorial/src/index.js
+++ react-basics-tutorial/src/index.js
@@ -13,17 +13,25 @@ const books = [
     title: "Our Class is a Family",
     author: "Shannon Olsen",
   },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71e5m7xQd0L._AC_UL200_SR200,200_.jpg",
+    title: "The Vanish Half: A Novel",
+    author: "Brit Bennett",
+  },
 ];
 
-const names = ["john", "peter", "susan"];
-const newNames = names.map((name) => <h1>{name}</h1>);
-
 function BookList() {
-  return <section className="booklist">{newNames}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => (
+        <Book book={book} />
+      ))}
+    </section>
+  );
 }
 
 const Book = (props) => {
-  const { img, title, author } = props;
+  const { img, title, author } = props.book;
 
   return (
     <article className="book">
