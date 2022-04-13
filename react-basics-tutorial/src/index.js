00389e84ea4673e0564bdaf6e6bec18b8f0b791a
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 14:22:03 -0300

    react: Props - Destructuring

--- react-basics-tutorial/src/index.js
+++ react-basics-tutorial/src/index.js
@@ -30,14 +30,14 @@ function BookList() {
   );
 }
 
-const Book = (props) => {
-  console.log(props);
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
 
   return (
     <article className="book">
-      <img src={props.img} alt="" />
-      <h1>{props.title}</h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="" />
+      <h1>{title}</h1>
+      <h4>{author}</h4>
     </article>
   );
 };
