9c1b03dce3c9b492e075046c9afc6194ee387900
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 12:13:14 -0300

    react: Mini Book Project

--- react-basics-tutorial/src/index.js
+++ react-basics-tutorial/src/index.js
@@ -1,17 +1,36 @@
 import ReactDOM from "react-dom";
 
-function Greeting() {
+function BookList() {
   return (
-    <div>
-      <Person />
-      <Message />
-    </div>
+    <section>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </section>
   );
 }
 
-const Person = () => <h2>john doe</h2>;
-const Message = () => {
-  return <p>this is my message</p>;
+const Book = () => {
+  return (
+    <article>
+      <Image></Image>
+      <Title />
+      <Author />
+    </article>
+  );
 };
+const Image = () => (
+  <img
+    src="https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg"
+    alt=""
+  />
+);
 
-ReactDOM.render(<Greeting />, document.getElementById("root"));
+const Title = () => <h1>I Love You to the Moon and Back</h1>;
+const Author = () => <h4>Amelia Hepworth</h4>;
+
+ReactDOM.render(<BookList />, document.getElementById("root"));
