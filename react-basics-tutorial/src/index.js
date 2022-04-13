b107ec380ebcca916ec425dac1fb403cc1dc02c3
Leonardo Xavier <gxavier.leonardo@gmail.com>
2022-04-13 14:44:29 -0300

    react: Simple List

--- react-basics-tutorial/src/index.js
+++ react-basics-tutorial/src/index.js
@@ -2,47 +2,32 @@ import "./index.css";
 import ReactDOM from "react-dom";
 
 // setup vars
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth",
-};
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
-  title: "Our Class is a Family",
-  author: "Shannon Olsen",
-};
+const books = [
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    img: "https://images-na.ssl-images-amazon.com/images/I/71aLultW5EL._AC_UL200_SR200,200_.jpg",
+    title: "Our Class is a Family",
+    author: "Shannon Olsen",
+  },
+];
+
+const names = ["john", "peter", "susan"];
+const newNames = names.map((name) => <h1>{name}</h1>);
 
 function BookList() {
-  return (
-    <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      >
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dicta,
-          sed at dolorum blanditiis obcaecati corporis perspiciatis qui
-          reiciendis enim.
-        </p>
-      </Book>
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 }
 
 const Book = (props) => {
-  const { children, img, title, author } = props;
+  const { img, title, author } = props;
 
   return (
     <article className="book">
       <img src={img} alt="" />
-      {children}
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
